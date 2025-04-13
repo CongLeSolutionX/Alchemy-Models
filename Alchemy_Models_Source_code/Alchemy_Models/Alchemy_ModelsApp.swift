adf3e0f639eef545465db5bee4419fa6a4e5c379
Cong Le <cle155@csu.fullerton.edu>
2025-04-13 10:10:55 -0700

    initial set up

---
+++ Alchemy_Models_Source_code/Alchemy_Models/Alchemy_ModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alchemy_ModelsApp.swift
+//  Alchemy_Models
+//
+//  Created by Cong Le on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Alchemy_ModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
